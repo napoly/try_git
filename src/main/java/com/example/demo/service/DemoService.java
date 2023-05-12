198814aa27cc7e73500d2a79ea115450be6feace
napoly <napolytan@protonmail.com>
2023-05-12 13:18:53 +0200

    Codacy gradle code coverage spring boot

---
+++ src/main/java/com/example/demo/service/DemoService.java
@@ -0,0 +1,10 @@
+package com.example.demo.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class DemoService {
+    public String helloWorld() {
+        return "hello world";
+    }
+}
