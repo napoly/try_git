8a8a997dd27f1436b8acdc9bfb3aebf133aa36bc
napoly <napolytan@protonmail.com>
2023-07-15 15:01:57 +0200

    Testing rebase with code coverage

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -14,7 +14,7 @@ class DemoApplicationTests {
 
     @Test
     void testHelloWorld() {
-        assertEquals("hello world", demoService.helloWorld());
+        assertEquals("hello world.. change for rebase", demoService.helloWorld());
     }
 
 }
